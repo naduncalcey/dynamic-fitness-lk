eab0adc4de7b6319574d048d71a6df6bfdf5d559
“nadundev” <nadunnissankauiux@gmail.com>
2024-04-11 13:34:30 +0530

    navbar created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        "brand": {
+          "primary": "rgb(var(--brand-primary))",
+          "secondary": "rgb(var(--brand-secondary))",
+          "ternary": "rgb(var(--brand-ternary))"
+        }
+      }
     },
   },
   plugins: [],
