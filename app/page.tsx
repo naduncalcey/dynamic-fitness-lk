d35136aea01df859e5cb976b272b61dda90ba1c8
Nadun Nissanka <110454669+naduncalcey@users.noreply.github.com>
2024-07-18 17:39:29 +0530

    Updated the paragraph text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    <h1>Dynamic Fitness</h1>
+    <h1 className="text-red-700 font-bold text-xl">Dynamic Fitness</h1>
+    <p className="text-gray-100 font-medium text-md">This is a gym website</p>
   </div>
   );
 }
