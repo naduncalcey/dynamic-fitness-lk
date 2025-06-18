4e1f48ab78f86ef0accda593a8c41221a296d389
nadundev <116334152+nadundev@users.noreply.github.com>
2025-06-18 14:11:35 +0530

    Adding the best web logo

--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -68,8 +68,8 @@ export default function Hero() {
                     <img
                       src="https://ebadge.bestweb.lk/eBadgeSystem/domainNames/dynamicfitness.lk/BestWeb/2025/Rate_Us/image.png"
                       alt="logo"
-                      width="150"
-                      height="150"
+                      width="100"
+                      height="100"
                     />
                   </a>
                   <h1 className="text-pretty text-5xl font-semibold tracking-tight text-white sm:text-7xl font-poppins">
